036fa302a3964275ef4659112a95e79c5a39fbf0
Joachim van de Haterd <derjoachim@gmail.com>
2020-03-22 13:31:39 +0100

    Read forecast_io key from config cache

--- config/services.php
+++ config/services.php
@@ -34,5 +34,9 @@ return [
         'key' => env('STRIPE_KEY'),
         'secret' => env('STRIPE_SECRET'),
     ],
+    
+    'forecast_io' => [
+        'key' => env('DARKSKY_NET_API_KEY')
+    ]
 
 ];
