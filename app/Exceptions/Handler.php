bf2c5f69d299e87d96a7e55d278bd227848edaef
Joachim van de Haterd <derjoachim@gmail.com>
2020-11-28 08:36:00 +0100

    Updated Laravel version to 7.X

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,7 @@
 
 namespace App\Exceptions;
 
-use Exception;
+use Throwable;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
@@ -21,32 +21,32 @@ class Handler extends ExceptionHandler
         \Illuminate\Session\TokenMismatchException::class,
         \Illuminate\Validation\ValidationException::class,
     ];
-
+    
     /**
      * Report or log an exception.
      *
      * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
      *
-     * @param  \Exception  $exception
+     * @param  \Throwable  $exception
      * @return void
      */
-    public function report(Exception $exception)
+    public function report(Throwable $exception)
     {
         parent::report($exception);
     }
-
+    
     /**
      * Render an exception into an HTTP response.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
+     * @param  \Throwable  $exception
      * @return \Illuminate\Http\Response
      */
-    public function render($request, Exception $exception)
+    public function render($request, Throwable $exception)
     {
         return parent::render($request, $exception);
     }
-
+    
     /**
      * Convert an authentication exception into an unauthenticated response.
      *
@@ -59,7 +59,27 @@ class Handler extends ExceptionHandler
         if ($request->expectsJson()) {
             return response()->json(['error' => 'Unauthenticated.'], 401);
         }
-
+        
         return redirect()->guest('login');
     }
+    
+    /**
+     * @param  Throwable  $exception
+     * @return bool|void
+     */
+    public function shouldReport(Throwable $exception)
+    {
+        parent::shouldReport($exception);
+    }
+    
+    
+    /**
+     * @param  \Symfony\Component\Console\Output\OutputInterface  $output
+     * @param  Throwable  $exception
+     */
+    public function renderForConsole($output, Throwable $exception)
+    {
+        parent::renderForConsole($output, $exception);
+    }
+    
 }
