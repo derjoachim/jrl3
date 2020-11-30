1237b72fb91ff3ae8d6aa0fa5a1b2cbc14e2566e
Joachim van de Haterd <derjoachim@gmail.com>
2020-11-30 18:15:34 +0100

    Added average kilometer time to workout detail screen and workout lists

--- app/Http/Controllers/WorkoutsController.php
+++ app/Http/Controllers/WorkoutsController.php
@@ -5,14 +5,15 @@ use App\Models\Workout;
 use App\Models\Route;
 use Auth;
 use Carbon\Carbon;
-use Config; 
+use Config;
 use DB;
 use Illuminate\Http\Request;
 use Redirect;
 use Validator;
 
-final class WorkoutsController extends Controller {
-
+final class WorkoutsController extends Controller
+{
+    
     protected $rules = [
         'date' => 'required|date',
         'name' => 'required|min:3',
@@ -20,15 +21,15 @@ final class WorkoutsController extends Controller {
         'avg_hr' => 'nullable|numeric|digits:3',
         'max_hr' => 'nullable|numeric|digits:3',
     ];
-
+    
     /**
-     * 
+     *
      */
     public function __construct()
     {
         $this->middleware('auth');
     }
-
+    
     /**
      * Display a listing of the resource.
      *
@@ -41,17 +42,17 @@ final class WorkoutsController extends Controller {
         
         // @TODO: This is a quick 'n dirty solution. There probably a better
         // solution
-        foreach( $workouts as $workout) {
+        foreach ($workouts as $workout) {
             $workout->time = $workout->getTime('time_in_seconds');
             $workout->route = trans('app.none');
             $route_id = $workout->route_id;
-            if($route_id > 0 && array_key_exists($route_id, $routes)) {
+            if ($route_id > 0 && array_key_exists($route_id, $routes)) {
                 $workout->route = $routes[$route_id];
             }
         }
         return view('workouts.index', compact('workouts'));
     }
-
+    
     /**
      * Show the form for creating a new resource.
      *
@@ -62,14 +63,14 @@ final class WorkoutsController extends Controller {
         $data = array();
         $data['routes'] = Route::getAllInArray();
         $data['date'] = date("Y-m-d");
-        return view('workouts.create',$data);
+        return view('workouts.create', $data);
     }
-
+    
     /**
      * Store a newly created resource in storage.
-     * 
-     * @param \App\Models\Workout $workout
-     * @param \Illuminate\Http\Request $request
+     *
+     * @param  \App\Models\Workout  $workout
+     * @param  \Illuminate\Http\Request  $request
      *
      * @return Response
      */
@@ -80,14 +81,14 @@ final class WorkoutsController extends Controller {
         $input['user_id'] = $request->user()->id; //hackish, pls refactor
         $workout = new Workout($input);
         $workout->save();
-
+        
         return Redirect::route('workouts.index')->with('message', trans('jrl.workout_saved'));
     }
-
+    
     /**
      * Display the specified resource.
      *
-     * @param  Workout $workout
+     * @param  Workout  $workout
      * @return Response
      */
     public function show(Workout $workout)
@@ -95,39 +96,38 @@ final class WorkoutsController extends Controller {
         $Data = array(
             'workout' => $workout,
             'id' => $workout->id,
-            't' => $workout->getTime(),
             'route' => null,
             'next' => null,
             'prev' => null
         );
-
-        if($workout->route_id > 0) {
+        
+        if ($workout->route_id > 0) {
             $Data['route'] = Route::find($workout->route_id)->name;
         }
         
-        $prev = Workout::where('id','<',$workout->id)->whereUserId(Auth::user()->id)->max('id');
-        if ( ! is_null($prev)) {
+        $prev = Workout::where('id', '<', $workout->id)->whereUserId(Auth::user()->id)->max('id');
+        if (!is_null($prev)) {
             $Data['prev'] = Workout::find($prev)->slug;
         }
-        $next = Workout::where('id','>',$workout->id)->whereUserId(Auth::user()->id)->min('id');
-        if ( ! is_null($next)) {
+        $next = Workout::where('id', '>', $workout->id)->whereUserId(Auth::user()->id)->min('id');
+        if (!is_null($next)) {
             $Data['next'] = Workout::find($next)->slug;
         }
         return view('workouts.show', $Data);
     }
-
+    
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  Workout $workout
-     * @param Request $request
-     * @return view 
+     * @param  Workout  $workout
+     * @param  Request  $request
+     * @return view
      * @todo: find a better way to handle mood and health
      */
     public function edit(Workout $workout, Request $request)
     {
         $Data = array();
-        if($workout->user_id == $request->user()->id) {
+        if ($workout->user_id == $request->user()->id) {
             $Data['workout'] = $workout;
             $Data['routes'] = Route::getAllInArray();
             $Data['mood'] = $workout->mood;
@@ -140,7 +140,7 @@ final class WorkoutsController extends Controller {
             return view('workouts.index')->with('message', trans('jrl.workout_not_authorized'));
         }
     }
-
+    
     /**
      * Update the specified resource in storage.
      *
@@ -151,28 +151,27 @@ final class WorkoutsController extends Controller {
     {
         $this->validate($request, $this->rules);
         $input = array_except($request->all(), '_method');
-        if(!array_key_exists('finished', $input)) {
+        if (!array_key_exists('finished', $input)) {
             $input['finished'] = 0;
         }
         $workout->update($input);
-        return Redirect::route('workouts.show',$workout->slug)->with('message',trans('jrl.workout_saved'));
+        return Redirect::route('workouts.show', $workout->slug)->with('message', trans('jrl.workout_saved'));
     }
-
+    
     /**
      * Remove the specified resource from storage.
      *
      * @param  Workout  $workout
-     * @param Request $request
+     * @param  Request  $request
      * @return Redirect
      */
     public function destroy(Workout $workout, Request $request)
     {
-        if($workout->user_id == $request->user()->id)
-        {
+        if ($workout->user_id == $request->user()->id) {
             $workout->delete();
-            return Redirect::route('workouts.index')->with('message',trans('jrl.workout_deleted'));
+            return Redirect::route('workouts.index')->with('message', trans('jrl.workout_deleted'));
         } else {
-            return Redirect::route('workouts.index')->with('message',trans('jrl.workout_not_authorized'));
+            return Redirect::route('workouts.index')->with('message', trans('jrl.workout_not_authorized'));
         }
     }
     
@@ -193,26 +192,26 @@ final class WorkoutsController extends Controller {
      * @return Redirect
      */
     public function parse(Request $request)
-	{
+    {
         $file = $request->get('file');
         $arrVal = array('file' => $file);
-        $validator = Validator::make($arrVal,[
-            'file' => ['required','mimes:xml,gpx']
+        $validator = Validator::make($arrVal, [
+            'file' => ['required', 'mimes:xml,gpx']
         ]);
-
-        if($validator->fails()) {
+        
+        if ($validator->fails()) {
             return Redirect::to('upload')->withInput()->withErrors($validator);
         }
-  
-        $destinationPath = base_path() . '/public/tmp/';
+        
+        $destinationPath = base_path().'/public/tmp/';
         $extension = $file->getClientOriginalExtension();
         $fileName = $file->getFileName().'.'.$extension;
         $file->move($destinationPath, $fileName);
         $json = json_encode(simplexml_load_file($destinationPath.$fileName));
         $data = json_decode($json);
-
+        
         $trkseg = $data->trk->trkseg->trkpt;
-
+        
         // Calculate an approximate distance in kilometers
         $distance = 0;
         $lastLon = null;
@@ -220,14 +219,14 @@ final class WorkoutsController extends Controller {
         // Also: calculate moving time
         $moving_time = 0;
         $lastTs = null;
-        foreach($trkseg as $pt) {
+        foreach ($trkseg as $pt) {
             $lat = $pt->{'@attributes'}->lat;
             $lon = $pt->{'@attributes'}->lon;
             $ts = $pt->time;
-
-            if(!is_null($lastLon) && !is_null($lastLat)) {    
+            
+            if (!is_null($lastLon) && !is_null($lastLat)) {
                 // Was there movement?
-                if( $lat != $lastLat || $lon != $lastLon ) {
+                if ($lat != $lastLat || $lon != $lastLon) {
                     $moving_time += Carbon::parse($ts)->diffInSeconds(Carbon::parse($lastTs));
                     $distance += $this->_calcDistance($lat, $lon, $lastLat, $lastLon);
                 }
@@ -236,8 +235,8 @@ final class WorkoutsController extends Controller {
             $lastLat = $lat;
             $lastTs = $ts;
         }
-        $distance = round($distance,2);
-
+        $distance = round($distance, 2);
+        
         // Start calculating elapsed time
         $first = $trkseg[0];
         $last = array_pop($trkseg);
@@ -245,17 +244,19 @@ final class WorkoutsController extends Controller {
         // GPX has its times stored in the UTC time zone. Convert it to your own.
         // Make sure that it is set in app/config.php!
         $tz = Config::get('app.timezone');
-        $ts = Carbon::parse($first->time,'UTC');
+        $ts = Carbon::parse($first->time, 'UTC');
         $ts->setTimezone($tz);
         
         // Try to determine a name. This fallback makes some sense
         $name = "GPX Import ".$ts->format('Y-m-d');
         // Strava exports GPX thus:
-        if(property_exists($data->trk, "name")) {
+        if (property_exists($data->trk, "name")) {
             $name = $data->trk->name;
-        } else if(property_exists($data->metadata, "name")) {
-            // MeeRun puts its name in the metadata
-            $name = $data->metadata->name;
+        } else {
+            if (property_exists($data->metadata, "name")) {
+                // MeeRun puts its name in the metadata
+                $name = $data->metadata->name;
+            }
         }
         
         $workout = new Workout();
@@ -274,7 +275,7 @@ final class WorkoutsController extends Controller {
         $workout_id = $workout->id;
         
         $arrWps = array();
-        foreach($trkseg as $t){
+        foreach ($trkseg as $t) {
             $arrWps[] = array(
                 'workout_id' => $workout_id,
                 'timestamp' => $t->time,
@@ -283,18 +284,18 @@ final class WorkoutsController extends Controller {
             );
         }
         DB::table('waypoints')->insert($arrWps);
-            
+        
         $workout = Workout::find($workout_id);
         // Dirty hack. Please refactor if needed
         return Redirect::to('workouts/'.$workout->slug.'/edit');
-	}
-
+    }
+    
     /**
      * Calculate the distance between two points in kilometers
      * Because fuck the imperial system :-P
-     * 
+     *
      * Source: http://stackoverflow.com/questions/27928/how-do-i-calculate-distance-between-two-latitude-longitude-points
-     * 
+     *
      * @param $lat1
      * @param $lon1
      * @param $lat2
@@ -308,14 +309,14 @@ final class WorkoutsController extends Controller {
         $lon1 *= $pi80;
         $lat2 *= $pi80;
         $lon2 *= $pi80;
-
+        
         $r = 6372.797; // mean radius of Earth in km
         $dlat = $lat2 - $lat1;
         $dlon = $lon2 - $lon1;
         $a = sin($dlat / 2) * sin($dlat / 2) + cos($lat1) * cos($lat2) * sin($dlon / 2) * sin($dlon / 2);
         $c = 2 * atan2(sqrt($a), sqrt(1 - $a));
         $km = $r * $c;
-
+        
         return $km;
     }
 }
--- app/Models/Workout.php
+++ app/Models/Workout.php
@@ -5,14 +5,15 @@ use Illuminate\Database\Eloquent\Model;
 use Cviebrock\EloquentSluggable\Sluggable;
 
 
-final class Workout extends Model {
-
+final class Workout extends Model
+{
+    
     protected $guarded = ['id'];
     
     protected $dates = ['date'];
     
     use Sluggable;
-
+    
     public function sluggable()
     {
         return [
@@ -27,20 +28,7 @@ final class Workout extends Model {
     {
         return 'slug';
     }
-//
-//    public function getEntryAttribute()
-//    {
-//        $dt = Carbon::parse($this->attributes['date']);
-//        $strdt = $dt->year.'-'.($dt->month < 10 ? '0': '').$dt->month.'-'.
-//            ($dt->day < 10 ? '0' : '').$dt->day;
-//        return $strdt. ' ' . $this->name;
-//    }
-//
-//    public function setDateAttribute($date)
-//    {
-//        $this->attributes['date'] = Carbon::createFromFormat('Y-m-d', $date);
-//    }
-
+    
     /*
      * Times are saved in seconds. This method converts this value in a readable 
      * format
@@ -50,10 +38,20 @@ final class Workout extends Model {
     public function getTime($fld = 'time_in_seconds')
     {
         $secs = $this->attributes[$fld];
-        $t = ($secs > 3600 ? 'H:' : '') . 'i:s';
+        $t = ($secs > 3600 ? 'H:' : '').'i:s';
         return gmdate($t, $secs);
     }
-
+    
+    /**
+     * Calculate and render average minutes per kilometer
+     *
+     * @return string
+     */
+    public function getAvg(): string
+    {
+        return gmdate('i:s', ($this->time_in_seconds / $this->distance));
+    }
+    
     public function setTimeInSecondsAttribute($time)
     {
         $this->attributes['time_in_seconds'] = $this->_time_to_seconds($time);
@@ -61,9 +59,9 @@ final class Workout extends Model {
     
     public function route()
     {
-        return $this->belongsTo('App\Models\Route','route_id');
+        return $this->belongsTo('App\Models\Route', 'route_id');
     }
-
+    
     public function users()
     {
         return $this->belongsTo('App\Models\User');
@@ -76,20 +74,20 @@ final class Workout extends Model {
     
     public function fitness_services() // Rename?
     {
-        return $this->hasOne('App\Models\FitnessService','workouts_fitness_services');
+        return $this->hasOne('App\Models\FitnessService', 'workouts_fitness_services');
     }
     
     public function workouts_fitness_services()
     {
         return $this->hasOne('App\Models\WorkoutsFitnessServices');
     }
-
+    
     private function _time_to_seconds($time)
     {
-        $arTime = array_reverse(explode(':',$time));
+        $arTime = array_reverse(explode(':', $time));
         $seconds = 0;
-        foreach( $arTime as $idx => $num) {
-            $seconds += ($num * pow(60,$idx));
+        foreach ($arTime as $idx => $num) {
+            $seconds += ($num * pow(60, $idx));
         }
         return $seconds;
     }
